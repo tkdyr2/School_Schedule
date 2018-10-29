cb4ac9e258d8f5081b8a4d1f0d4b3beb9652f996
pokipoki1245 <overman12344321@gmail.com>
2018-10-29 20:53:53 +0900

    add field and empty method. I will add the content in the empty method at next.

--- app/src/main/java/cookandroid/com/schoolschedule/SearchClasses.java
+++ app/src/main/java/cookandroid/com/schoolschedule/SearchClasses.java
@@ -1,8 +1,27 @@
 package cookandroid.com.schoolschedule;
 
+import java.util.ArrayList;
+
 public class SearchClasses {
 
+    String majors, kindOfClass, grade, classNsme, Mon, Fri, PM;
 
+    public ArrayList searchTheClass(String majors,String kindOfClass,String grade,String classNsme,String Mon,String Fri,String PM){
+
+
+
+
+
+
+
+
+
+
+
+
+        ArrayList<String> result = new ArrayList<>();
+        return result;
+    }
 
 
 
